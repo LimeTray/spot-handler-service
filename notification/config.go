d585d089ef1de3ec0358f6c3a2470a4c53f2f007
Shubham Negi <shubham.negi@semaai.com>
2021-11-23 11:37:44 +0530

    update default values

--- notification/config.go
+++ notification/config.go
@@ -3,9 +3,9 @@ package notification
 import "os"
 
 var (
-	DEFAIULT_SLACK_URL      string = "#default"
-	DEFAIULT_SLACK_USERNAME string = "#default"
-	DEFAIULT_SLACK_CHANNEL  string = "#default"
+	DEFAIULT_SLACK_URL      string = "https://hooks.slack.com/services/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"
+	DEFAIULT_SLACK_USERNAME string = "spot-handler-service"
+	DEFAIULT_SLACK_CHANNEL  string = "#prod-infra-alerts"
 )
 
 func Getenv(key, fallback string) string {
